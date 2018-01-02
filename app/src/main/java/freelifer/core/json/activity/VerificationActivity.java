c0928c6e74eeb7dfff638c8aaeb8c6473b65140d
pig <576618121@qq.com>
2018-01-02 10:52:43 +0800

    add VerificationActivity page

--- app/src/main/java/freelifer/core/json/activity/VerificationActivity.java
+++ app/src/main/java/freelifer/core/json/activity/VerificationActivity.java
@@ -1,7 +1,11 @@
 package freelifer.core.json.activity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import freelifer.core.json.R;
 import freelifer.core.json.bean.Goods;
@@ -21,7 +25,8 @@ public class VerificationActivity extends BaseActivity {
 
         srcDataText = find(R.id.tv_src_data);
         dstDataText = find(R.id.tv_dst_data);
-        start();
+//        start();
+        time();
     }
 
     private void start() {
@@ -29,10 +34,31 @@ public class VerificationActivity extends BaseActivity {
         srcDataText.setText(srcData);
 
         Goods goods = Goods$$CREATOR.create(srcData, false);
-
         dstDataText.setText(goods.toString());
 
     }
 
+    private void time() {
+        String srcData = readAssetsFile("goods.json");
+        srcDataText.setText(srcData);
+        Gson gson = new GsonBuilder().create();
+        int count = 10000;
+
+        long start = SystemClock.elapsedRealtime();
+        for (int i = 0; i < count; i++) {
+            Goods goods = Goods$$CREATOR.create(srcData, false);
+        }
+        long end = SystemClock.elapsedRealtime();
+        long selfTime = end - start;
+
+
+        start = SystemClock.elapsedRealtime();
+        for (int i = 0; i < count; i++) {
+            gson.fromJson(srcData, Goods.class);
+        }
+
+        end = SystemClock.elapsedRealtime();
+        dstDataText.setText(String.format("LimitJSON time: %d\n Gson Time: %d", selfTime, (end - start)));
+    }
 
 }
